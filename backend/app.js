e91a93a542df3d2a553f42405f2613ccf22edeac
Hussain Jamal <Hjamal9865@gmail.com>
2025-09-04 09:51:33 +0500

    user model created

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 const  express = require("express")
 const dotenv = require("dotenv")
+const ErrorHandler = require ( "./utils/errorHandler.js")
+const cookieParser = require ( "cookie-parser")
 const app =express()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     dotenv.config({
         path:"backend/config/.env"
     })
 }
+app.use(ErrorHandler)
+
+
 
 module.exports =  app
