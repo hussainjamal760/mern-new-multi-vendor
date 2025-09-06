403994d48d808c2fcd3f6f6fe8a3879be6001d85
Hussain Jamal <Hjamal9865@gmail.com>
2025-09-06 22:58:46 +0500

    hero section added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const HomePage = () => {
   return (
     <>
         <Header activeHeading={1}/>
+        <Hero/>
     </>
   )
 }
